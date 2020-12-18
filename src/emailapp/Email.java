f66a3d3018ac3437b8c309557332262e781c970f
Dilan Loyola <ldilan6@gmail.com>
2020-12-17 23:18:39 -0500

    Worked on constructor, asked for department.

---
+++ src/emailapp/Email.java
@@ -0,0 +1,52 @@
+package emailapp;
+
+import java.util.Scanner;
+
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail;
+	
+	// Constructor to receive the first name and last name
+	public Email(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("EMAIL CREATED: "+ this.firstName + " " + this.lastName);
+		
+		// Call a method asking for the department - return the department 
+		this.department = setDepartment();
+		System.out.println("Department: "+ this.department);
+	}
+	
+	// Ask for the department
+	private String setDepartment() {
+		System.out.print("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
+		Scanner in = new Scanner(System.in);
+		int depChoice = in.nextInt();
+		if(depChoice == 1) {
+			return "sales";
+		}
+		else if(depChoice == 2) {
+			return "development";
+		}
+		else if(depChoice == 3) {
+			return "accounting";
+		}
+		else {
+			return "";
+		}
+	}   
+	
+	// Generate a random password
+	
+	// Set the mailbox capacity
+	
+	// Set alternate email
+	
+	// Change the password
+	
+	
+}
