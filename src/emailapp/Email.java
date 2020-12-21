81f831d6820f85832520bd77c3b3296f38c61d91
Dilan Loyola <ldilan6@gmail.com>
2020-12-20 23:11:57 -0500

    create email and set create random password.

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -7,8 +7,11 @@ public class Email {
 	private String lastName;
 	private String password;
 	private String department;
+	private String email;
 	private int mailboxCapacity;
+	private int defaultpasswordlength = 10;
 	private String alternateEmail;
+	private String companySuffix = "company.com";
 	
 	// Constructor to receive the first name and last name
 	public Email(String firstName, String lastName) {
@@ -19,6 +22,14 @@ public class Email {
 		// Call a method asking for the department - return the department 
 		this.department = setDepartment();
 		System.out.println("Department: "+ this.department);
+		
+		// Call a method that returns a random password
+		this.password = randomPassword(defaultpasswordlength);
+		System.out.println("Your password is: "+ this.password);
+		
+		// Combine elements to generate email
+		email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix; 
+		System.out.println("Your email is: " + email);
 	}
 	
 	// Ask for the department
@@ -41,6 +52,15 @@ public class Email {
 	}   
 	
 	// Generate a random password
+	private String randomPassword(int length) {
+		String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+		char[] password = new char[length];
+		for(int i = 0; i < length; i++) {
+			int rand = (int) (Math.random() * passwordSet.length());
+			password[i] = passwordSet.charAt(rand);
+		}
+		return new String (password);
+	}
 	
 	// Set the mailbox capacity
 	
